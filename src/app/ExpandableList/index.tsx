517ba5d9bdf14abbd0a95dd30e45dd349358832e
Lin Lan <lanlin@meideng.net>
2021-08-08 12:35:37 +0800

    Step3-1: 父元素样式

--- src/app/ExpandableList/index.tsx
+++ src/app/ExpandableList/index.tsx
@@ -1,32 +1,45 @@
 import * as React from 'react';
 import { ReactNode } from 'react';
+import styles from './index.scss';
+import classNames from 'classnames';
 
 export type ExpandableListNode<T> = {
-  children: ExpandableListNode<T>[]
-  data: T,
-  defaultExpand?: boolean,
-}
-
+  id: string;
+  children: ExpandableListNode<T>[];
+  data: T;
+  defaultExpand?: boolean;
+};
 
 interface ExpandableListProps<T> {
   root: ExpandableListNode<T>;
   render?: (root: ExpandableListNode<T>) => ReactNode;
+  selectedId: string;
+  setSelectedId: (v: string) => void;
 }
 
 export function ExpandableList<T>(props: ExpandableListProps<T>) {
-  const { root, render } = props;
+  const { root, render, selectedId, setSelectedId } = props;
   const [expand, setExpand] = React.useState<boolean>(!!root.defaultExpand);
-  const handleClick = React.useCallback(() => {
+  const handleExpand = React.useCallback(() => {
     setExpand(!expand);
   }, [expand]);
+  const handleClick = React.useCallback(() => {
+    setSelectedId(root.id);
+  }, [root]);
 
-  return <div>
-    <div onClick={handleClick}>
-      {render ? render(root) : <p>{root.data}</p>}
+  return (
+    <div className={styles.root}>
+      <div onClick={handleClick} className={classNames(styles.parent, selectedId === root.id && styles.selected)}>
+        {render ? render(root) : <span>{root.id}</span>}
+      </div>
+      <div
+        className={classNames(styles.children, expand || styles.hide)}
+        style={{ height: (expand ? 24 * root.children.length : 0) + 'px' }}
+      >
+        {root.children.map((node) => (
+          <ExpandableList root={node} render={render} selectedId={selectedId} setSelectedId={setSelectedId} />
+        ))}
+      </div>
     </div>
-    {expand && <div>
-      {root.children.map(node => <ExpandableList root={node} render={render} />)}
-    </div>
-    }
-  </div>;
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -17,9 +17,9 @@ function MyList() {
   const root = React.useMemo<ExpandableListNode<SomeCategory>>(() => {
     // 1. 先把列表转化为Map，把所有元素注册进去
     const map = new Map<string, ExpandableListNode<SomeCategory>>();
-    cats.forEach(cat => map.set(cat.id, { children: [], data: cat }));
+    cats.forEach(cat => map.set(cat.id, { id: cat.id, children: [], data: cat }));
     // 2. 兜底处理一下根节点，应对列表为空的情况
-    map.set('0', map.get('0') || { children: [], data: { id: '0', parentId: '', title: '根目录' } });
+    map.set('0', map.get('0') || { id: '0', children: [], data: { id: '0', parentId: '', title: '根目录' } });
     // 3. 逐个添加到父节点的children中去
     cats.forEach(cat => map.get(cat.parentId)?.children.push(map.get(cat.id)));
     // 4. 根节点默认展开
@@ -27,10 +27,11 @@ function MyList() {
     return map.get('0');
   }, [cats]);
   const render = React.useCallback((root: ExpandableListNode<SomeCategory>) => {
-    return <p>{root.data.title}</p>;
+    return <span>{root.data.title}</span>;
   }, []);
+  const [selectedId, setSelectedId] = React.useState('0');
 
   return <div>
-    <ExpandableList root={root} render={render} />
+    <ExpandableList root={root} render={render} selectedId={selectedId} setSelectedId={setSelectedId} />
   </div>;
 }
