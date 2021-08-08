8d25867f0d45044085038deddef853ff6ce94068
Lin Lan <lanlin@meideng.net>
2021-08-08 14:04:06 +0800

    Step3-2: 前缀树枝样式

--- src/app/ExpandableList/index.tsx
+++ src/app/ExpandableList/index.tsx
@@ -15,10 +15,12 @@ interface ExpandableListProps<T> {
   render?: (root: ExpandableListNode<T>) => ReactNode;
   selectedId: string;
   setSelectedId: (v: string) => void;
+  depth?: number;
+  isLast?: boolean;
 }
 
 export function ExpandableList<T>(props: ExpandableListProps<T>) {
-  const { root, render, selectedId, setSelectedId } = props;
+  const { root, render, selectedId, setSelectedId, depth = 0, isLast } = props;
   const [expand, setExpand] = React.useState<boolean>(!!root.defaultExpand);
   const handleExpand = React.useCallback(() => {
     setExpand(!expand);
@@ -27,19 +29,46 @@ export function ExpandableList<T>(props: ExpandableListProps<T>) {
     setSelectedId(root.id);
   }, [root]);
 
+  const switchStyles = React.useMemo<string>(() => {
+    if (!root.children.length) return classNames(styles.prefix, styles.switch, styles.switch0);
+    if (expand) return classNames(styles.prefix, styles.switch, styles.switch2);
+    return classNames(styles.prefix, styles.switch, styles.switch1);
+  }, [root, expand]);
+  const branchStyles = React.useMemo<string>(() => {
+    if (depth === 0) return classNames(styles.prefix, styles.branch, styles.branch0);
+    if (isLast) return classNames(styles.prefix, styles.branch, styles.branch2);
+    return classNames(styles.prefix, styles.branch, styles.branch1);
+  }, [depth]);
+  const branchLines = React.useMemo<number[]>(
+    () => new Array(Math.max(depth - 1, 0)).fill(0).map((n, i) => i + 1),
+    [depth],
+  );
+
   return (
     <div className={styles.root}>
+      <div className={switchStyles} onClick={handleExpand} />
+      <div className={branchStyles} />
+      {branchLines.map((n) => (
+        <div className={classNames(styles.prefix, styles.branch, styles.branch3)} style={{ left: -n * 24 + 'px' }} />
+      ))}
       <div onClick={handleClick} className={classNames(styles.parent, selectedId === root.id && styles.selected)}>
         {render ? render(root) : <span>{root.id}</span>}
       </div>
-      <div
-        className={classNames(styles.children, expand || styles.hide)}
-        style={{ height: (expand ? 24 * root.children.length : 0) + 'px' }}
-      >
-        {root.children.map((node) => (
-          <ExpandableList root={node} render={render} selectedId={selectedId} setSelectedId={setSelectedId} />
-        ))}
-      </div>
+      {expand && (
+        <div className={styles.children}>
+          {root.children.map((node, index) => (
+            <ExpandableList
+              root={node}
+              render={render}
+              selectedId={selectedId}
+              setSelectedId={setSelectedId}
+              depth={depth + 1}
+              key={node.id}
+              isLast={index === root.children.length - 1}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
